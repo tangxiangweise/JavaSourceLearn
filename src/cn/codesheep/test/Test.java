2333fc2614e92b288fe89456506135a4c5aa468b
tangxiangwei <tangxiangwei@anfubaoxian.com>
2020-05-22 00:12:12 +0800

    Initial commit

---
+++ src/cn/codesheep/test/Test.java
@@ -0,0 +1,15 @@
+package cn.codesheep.test;
+
+import java.util.HashMap;
+
+/**
+ * @author tangxiangwei
+ * @date 20/3/26
+ */
+public class Test {
+    
+    public static void main(String[] args){
+        HashMap<Object, Object> obs = new HashMap<>();
+
+    }
+}
